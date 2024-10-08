61c666a19e6b01a4163bfb7c4d8f0883a466b55e
VoQuocViet4323 <140177566+VoQuocViet4323@users.noreply.github.com>
2024-10-08 21:45:14 +0700

    a

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] hocsinh = {"Viet", "Chien", "Vu", "Nam", "Nguyen", "Nga", "Linh", "Tran Anh"};
+        Console.Write($"Nhap ten hoc sinh: ");
+        string tenHocSinh = Console.ReadLine();
+        bool duyetmang = false;
+        for(int i = 0; i < hocsinh.Length; i++){
+            if(hocsinh[i].Equals(tenHocSinh)){
+                Console.WriteLine($"Hoc sinh '{tenHocSinh}' co trong mang.");
+                duyetmang = true;
+                break;
+            }
+        }
+        if(!duyetmang){
+            Console.WriteLine($"Hoc sinh '{tenHocSinh}' khong co trong mang.");
+        }
+    }
+}
